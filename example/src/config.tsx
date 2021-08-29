ee4f7670e56b37cad6d4266545937b23f9146f59
yuanguandong <995749721@qq.com>
2021-08-30 07:43:25 +0800

    chore: update

---
+++ example/src/config.tsx
@@ -0,0 +1,8 @@
+
+
+export default {
+  packageName:'react-keyevent',
+  name:'react-keyevent',
+  homePageUrl: 'https://github.com/yuanguandong/react-keyevent',
+  authorHomePage:'https://github.com/yuanguandong',
+}
